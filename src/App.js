55afbf6b462a2b04205f0082e76c5128e0bd5aaf
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-11-13 13:27:30 +0530

    Created routes in App.js, added links to navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router=createBrowserRouter([
+    {path: '/', element: <Nav/>,
+      children:[
+        {index: true, element:<Hero/>},
+        {path: '/courses', element:<Courses/>}
+      ]}
+  ])
   return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <Courses />
-    </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import style from "./Nav.module.css";
+import { Link, Outlet } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
-      <div className={style.nav_container}>
-        <div className={style.nav_title_wrapper}>
-          <img
-            className={style.logo}
-            src="https://files.codingninjas.in/pl-ninja-16706.svg"
-            alt="logo"
-          />
-          <h4>Coding Ninjas</h4>
+    <>
+      <nav>
+        <div className={style.nav_container}>
+          <div className={style.nav_title_wrapper}>
+            <Link to='/'><img
+              className={style.logo}
+              src="https://files.codingninjas.in/pl-ninja-16706.svg"
+              alt="logo"
+            /></Link>
+            <h4>Coding Ninjas</h4>
+          </div>
+          <div className={style.nav_details}>
+            <Link to='/courses'><button>Courses</button></Link>
+            
+          </div>
         </div>
-        <div className={style.nav_details}>
-          <button>Courses</button>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      <Outlet/>
+    </>
+
   );
 }
 
